d016634c2431e2dbec317e93705f6fcdc370927d
Max Fortun <max@maxf.net>
2018-02-09 19:41:39 -0500

    Cleaned up logging.

--- get-magnets.sh
+++ get-magnets.sh
@@ -2,32 +2,42 @@
 file=${1}
 
 while read name seq suffix; do
-	if [[ "$seq" =~ ^S([0-9]{2})E([0-9]{2}) ]]; then
-		S=${BASH_REMATCH[1]}
-		E=${BASH_REMATCH[2]}
-	fi
-	while ls *$name*$seq* ; do
+	checkSeq=true
+	while [ "$checkSeq" = "true" ]; do
+		if [[ "$seq" =~ ^S([0-9]{2})E([0-9]{2}) ]]; then
+			S=${BASH_REMATCH[1]}
+			E=${BASH_REMATCH[2]}
+		fi
+
 		E=${E##0}
 		E=0$(( E + 1 ))
 		L=${#E}
 		L=$(( L - 2 ))
 		E=${E:$L}
-		seq=S${S}E${E}
+		nextSeq=S${S}E${E}
+
+		if grep "$name.*$seq.*$suffix" $file.mags; then
+			echo "$name $seq $suffix exists. Next seq is $nextSeq."
+			seq=$nextSeq
+			continue
+		fi
+
+		echo "Looking for $name $seq $suffix."
+		url="https://www.thepiratebay.org/search/$name%20$seq%20$suffix/0/99/200" 
+		#echo "$url"
+		magnet=$(curl -s"$url" | grep -m1 -o 'magnet:[^"]*')
+		if [ -n "$magnet" ]; then
+			echo "$magnet"
+			echo "$magnet" >> $file.mags
+			echo "$name $seq $suffix found. Next seq is $nextSeq."
+			seq=$nextSeq
+		else
+			echo "$name $seq $suffix not found."
+			checkSeq=false
+		fi
+
+		echo "$name $seq $suffix" >> $file.new
 	done
-
-	if grep $name$seq $file.mags; then
-		continue
-	fi
-	url="https://www.thepiratebay.org/search/$name%20$seq%20$suffix/0/99/200" 
-	echo "$url"
-	magnet=$(curl "$url" | grep -m1 -o 'magnet:[^"]*')
-	if [ -n "$magnet" ]; then
-		echo "$magnet"
-		echo "$magnet" >> $file.mags
-	fi
-
-	echo "$name $seq $suffix" >> $file.new
-
 done < $file
 
 mv $file $file.bak
